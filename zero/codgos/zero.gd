a440ba83ea51c26b7b41dec88fd2b0c13cc02993
tanjirino07 <mercesvitor05@hotmail.com>
2024-07-23 11:35:24 -0300

    atualização escalada
    agora o zero possui a habilidade de escalar

--- zero/codgos/zero.gd
+++ zero/codgos/zero.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 # Variáveis de movimento
 var carregar = 0
 
+var wall_slide = false
 var velocity = Vector2.ZERO
 var gravity = 1000
 var speed = 200
@@ -14,6 +15,8 @@ var is_dashing = false
 var has_dashed = false  # Flag para controlar se o personagem já realizou um dash após tocar no chão
 var is_attacking = false  # Flag para controlar se o personagem está atacando
 var is_sabre_attacking = false  # Flag para controlar se o personagem está atacando com o sabre
+var is_wall_sliding = false  # Flag para controlar se o personagem está deslizando na parede
+var wall_slide_speed = 50  # Ajuste este valor conforme necessário
 
 # Tempo de dash
 var dash_time = 0.3
@@ -49,6 +52,10 @@ enum State { IDLE, RUNNING, JUMPING, FALLING, DASHING, ATTACKING, SABRE_ATTACKIN
 var current_state = State.IDLE
 
 func _physics_process(delta):
+	# Atualize a velocidade de queda durante o deslize na parede
+	if is_wall_sliding:
+		velocity.y = min(velocity.y, wall_slide_speed)
+
 	Global.carga = carregar
 	if Input.is_action_pressed("a"):
 		carregar += 1
@@ -84,7 +91,7 @@ func _physics_process(delta):
 		if dash_timer <= 0:
 			is_dashing = false
 		else:
-			if Input.is_action_pressed("z"):
+			if Input.is_action_pressed("z") and $AnimatedSprite.get_animation() != "parede":
 				$jump.play()
 				velocity.y = dash_jump_strength
 				can_double_jump = false  # Bloqueia o pulo duplo após o dash
@@ -102,15 +109,15 @@ func _physics_process(delta):
 	if is_on_floor():
 		can_double_jump = true
 		has_dashed = false  # Permitir dash infinito enquanto estiver no chão
-
-		if Input.is_action_just_pressed("z"):
-			$jump.play()
-			if is_dashing:
-				velocity.y = dash_jump_strength
-				can_double_jump = false  # Bloqueia o pulo duplo após o dash
-			else:
-				velocity.y = jump_strength
-			current_state = State.JUMPING
+		if $AnimatedSprite.get_animation() != "parede":
+			if Input.is_action_just_pressed("z"):
+				$jump.play()
+				if is_dashing:
+					velocity.y = dash_jump_strength
+					can_double_jump = false  # Bloqueia o pulo duplo após o dash
+				else:
+					velocity.y = jump_strength
+				current_state = State.JUMPING
 	else:
 		if Input.is_action_just_pressed("z") and can_double_jump and not has_dashed:
 			$jump.play()
@@ -131,7 +138,7 @@ func _physics_process(delta):
 		return
 
 	# Permitir movimento durante o ataque no ar
-	if (Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")) and (is_sabre_attacking and (current_state == State.JUMPING or current_state == State.FALLING)):
+	if (Input.is_action_pressed("ui_right") or Input.is_action_pressed("ui_left")) and (is_sabre_attacking and (current_state == State.JUMPING or current_state == State.FALLING)) and wall_slide == false:
 		if Input.is_action_pressed("ui_right"):
 			velocity.x = speed
 			move_direction = 1
@@ -170,19 +177,56 @@ func _physics_process(delta):
 
 	velocity = move_and_slide(velocity, Vector2.UP)
 
+	 # atualizar a animação
 	update_animation()
 
 	# Atualizar o tempo desde o último clique
 	time_since_last_click += delta
 	if time_since_last_click > double_click_threshold:
 		click_count = 0  # Redefine o contador de cliques após o limite de tempo
+		
+	# Mecanica de escalar
+	if not is_on_floor() and is_on_wall() and velocity.y > 0:
+		start_wall_slide()
+		can_double_jump = false
+		if Input.is_action_pressed("ui_left"):
+			move_direction = 1
+			is_wall_sliding = true
+			velocity.y += 30
+			if Input.is_action_just_pressed("z"):
+				$jump.play()
+				velocity.y -= 500
+				velocity.x -= 500
+		elif Input.is_action_pressed("ui_right"):
+			move_direction = -1
+			is_wall_sliding = true
+			velocity.y += 30
+			if Input.is_action_just_pressed("z"):
+				$jump.play()
+				velocity.y -= 500
+				velocity.x += 500
+	else:
+		stop_wall_slide()
 
+	if is_wall_sliding:
+		velocity.y = min(velocity.y, wall_slide_speed)
 
+	# mecanica de animação "parede" de escalar
+func start_wall_slide():
+	is_wall_sliding = true
+	$AnimatedSprite.play("parede")
+
+func stop_wall_slide():
+	is_wall_sliding = false
+
+	 # animações
 func update_animation():
 	if not $AnimatedSprite:
 		return
 
-	if is_attacking:
+	if is_wall_sliding:
+		$AnimatedSprite.play("parede")
+	elif is_attacking:
 		if not is_on_floor():
 			if velocity.y < 0:
 				$AnimatedSprite.play("atirando e pulando")
@@ -214,65 +258,67 @@ func update_animation():
 			$AnimatedSprite.play("parado")
 		else:
 			$AnimatedSprite.play("andar")
+		if is_on_wall() and velocity.y > 0:
+			$AnimatedSprite.play("parede")
 
+	# mecanicas de attack
 func _input(event):
+	# atirar 
 	if event.is_action_pressed("a") and not is_attacking and not is_sabre_attacking:
 		attack()
 
+	# Zsabre
 	if event.is_action_pressed("s") and not is_attacking and not is_sabre_attacking:
 		if is_on_floor():
 			ground_sabre_attack()
 		else:
 			air_sabre_attack()
 
-func sabre_attack():
-	if !is_on_floor() or velocity.y != 0:
-		air_sabre_attack()
-	else:
-		ground_sabre_attack()
-
+	# função de atirar
 func shoot():
 	var projectile = projectile_scene.instance()
 	projectile.position = position + Vector2(move_direction * 10, -5)  # Ajuste a posição de spawn do projétil
 	projectile.direction = Vector2(move_direction, 0)  # Define a direção do projétil
 	get_parent().add_child(projectile)
 
+	# controla a animação de atirar
 func attack():
 	is_attacking = true
 	attack_timer = attack_duration
 	current_state = State.ATTACKING
 	$AnimatedSprite.play("atirando")
 	shoot()
-	
-func stop_movement():
-	velocity = Vector2.ZERO  # Parar qualquer movimento horizontal
-	move_and_slide(velocity, Vector2.UP)
 
+	# para os movimentos do ZERO
+func stop_movement():
+	velocity = Vector2.ZERO  # Zera a velocidade para parar qualquer movimento
+	move_and_slide(velocity, Vector2.UP)  # Aplica o movimento com a nova velocidade zerada
+
+	# usar sabre no ar
 func air_sabre_attack():
-	# Verifica se o personagem está no ar (pulando ou caindo)
 	if !is_on_floor() or velocity.y != 0:
 		is_sabre_attacking = true
 		sabre_attack_timer = sabre_attack_duration
 		click_count += 1
-		if click_count == 1:
-			$AnimatedSprite.play("pulando zsabre")
-			yield(get_tree().create_timer(animation_duration), "timeout")
+		$AnimatedSprite.play("pulando zsabre")
+		yield(get_tree().create_timer(animation_duration), "timeout")
 		# Reseta click_count após o ataque
 		click_count = 0
 
+	# usar sabre no chão
 func ground_sabre_attack():
 	# Faz o ataque de sabre no chão
 	is_sabre_attacking = true
 	sabre_attack_timer = sabre_attack_duration
 	click_count += 1
+	stop_movement()  # Parar qualquer movimento
+
 	if click_count == 1:
-		stop_movement()  # Parar qualquer movimento
 		$AnimatedSprite.play("parado zsabre 1")
-		yield(get_tree().create_timer(animation_duration), "timeout")
-		if click_count == 1:  # Se ainda for um único clique após o tempo de animação
-			click_count = 0  # Reseta o contador de cliques
 	elif click_count == 2:
-		stop_movement()  # Parar qualquer movimento
 		$AnimatedSprite.play("parado zsabre 2")
-		yield(get_tree().create_timer(animation_duration), "timeout")
+		click_count = 0
+
+	yield(get_tree().create_timer(animation_duration), "timeout")
+	if click_count == 1:  # Se ainda for um único clique após o tempo de animação
 		click_count = 0  # Reseta o contador de cliques
